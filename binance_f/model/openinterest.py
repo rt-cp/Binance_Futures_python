dd3683096456b3b6b7023fbb651da75371a74e2b
rt-cp <105331823+rt-cp@users.noreply.github.com>
2022-05-26 21:25:26 +0800

    Update openinterest.py
    fixed get open interest missing response parameters

--- binance_f/model/openinterest.py
+++ binance_f/model/openinterest.py
@@ -5,11 +5,13 @@ class OpenInterest:
     def __init__(self):
         self.symbol = ""
         self.openInterest = 0.0
+        self.time = 0
     
     @staticmethod
     def json_parse(json_data):
         result = OpenInterest()
         result.symbol = json_data.get_string("symbol")
         result.openInterest = json_data.get_float("openInterest")
+        result.time = json_data.get_int("time")
 
         return result
